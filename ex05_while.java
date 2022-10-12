73884a2bc35e081925e744f3fbdaaaf24d066229
0441234 <114965536+0441234@users.noreply.github.com>
2022-10-12 16:13:56 +0900

    Add files via upload

---
+++ ex05_while.java
@@ -0,0 +1,18 @@
+package day03;
+
+public class ex05_while {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		int i =0;
+		while(i<10) {
+			i++;
+			sum += i;
+			System.out.print("i :"+i );
+			System.out.println(" Sum :" +sum);
+		}
+
+	}
+
+}
---
+++ ex08.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class ex08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=(char)96;
+		do {
+			a++;
+			System.out.print((char)a);
+		}while(a<=121);//97을 122까지
+
+	}
+
+}
